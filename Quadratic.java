4aed7ee2d51eb06e1caea10f1799694d578f6e25
jeffbai996 <j.bai@wustl.edu>
2023-08-02 13:24:10 -0700

    Complete quadratic calculator exercise

--- Quadratic.java
+++ Quadratic.java
@@ -1,13 +1,30 @@
+import java.util.Scanner;
 
 /**
- * Write a description of class Quadratic here.
+ * Quadratic equation calculator
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Jeff Bai
+ * @version 0.1
  */
 public class Quadratic
 {
     public static void main(String[] args) {
+        int a, b, c; // declare variables a, b, c
         
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Enter the value for a (the coefficient of x squared): ");
+        a = scan.nextInt();
+        
+        System.out.print("Enter the value for b (the coefficient of x): ");
+        b = scan.nextInt();
+        
+        System.out.print("Enter the value for c (the constant term): ");
+        c = scan.nextInt();
+        
+        double root1 = (-1 * b + Math.sqrt(Math.pow(b, 2) - 4 * a * c));
+        double root2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - 4 * a * c));
+        
+        System.out.println("The roots are: " + root1 + ", and: " + root2);
     }
 }
