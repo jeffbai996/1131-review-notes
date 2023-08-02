ee43c2685ba34d99da7536da7a10512a8df472b5
jeffbai996 <j.bai@wustl.edu>
2023-08-02 13:14:59 -0700

    Update object notes, add quadratic exercise

--- Objects.java
+++ Objects.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 /**
  * Write a description of class Objects here.
@@ -20,7 +21,49 @@ public class Objects
         
         System.out.println(name2); // => "Bob", however, this did not create
         // a new object!! name2 simply points to the same object as name1.
+        // These are known as 'aliases'
         
+        // initializing a String with new
+        String str1;
+        str1 = new String("I'm a string!");
         
+        // However, strings are so crucial that we can implicitly 
+        // instantiate one
+        
+        String str2 = "I'm an implicitly instantiated string";
+        
+        // The String class
+        // Strings are immutable
+        String str3 = "A string";
+        // some methods
+        str3.charAt(0); // returns character at index
+        str3.compareTo(str2); // is this string smaller/equal/larger
+        str3.equals(str2); // boolean comparison
+        
+        // Packages
+        // You can 'fully qualify' a method call, i.e. hash out the
+        // entire name of it to invoke without importing packages
+        // Only methods of java.lang.* are available by default
+        // Import classes with the 'import' keyword
+        // e.g. : import java.util.Random;
+        
+        // the Random class (java.util.Random)
+        Random rng = new Random(); // instantiate a Random object w ref 'rng'
+        int rand1 = rng.nextInt(10); // returns rand from 0 to 9
+        int rand2 = rng.nextInt(10) + 10; // returns rand from 10 to 19
+        // .nextFloat() does not take any parameters
+        float rand3 = rng.nextFloat(); // returns float from 0.0 to 1.0(exc.)
+        
+        /*  The Math class (java.lang.Math)
+         *  Does not need to be imported (part of java.lang)
+         *  This class is wholly comprised of 'static methods', i.e. those
+         *  where you don't have to instantiate a new instance of the class
+         *  in order to invoke. 
+        */
+        
+        int negative = -15;
+        System.out.println(Math.abs(negative)); // 15
+        double frac = 3 / 4;
+        System.out.println(Math.acos(frac)); // 1.57...
     }
 }
