036556075e2f44d946195b01f47a687251fcdb5d
jeffbai996 <j.bai@wustl.edu>
2023-08-02 13:50:01 -0700

    Complete enums note

--- Enums.java
+++ Enums.java
@@ -7,5 +7,19 @@
  */
 public class Enums
 {
+    // Create enumerated type Flavor
+    // enums are defined outside of the main class
+    enum Flavor {vanilla, chocolate, strawberry, coffee, cookiesAndCream, rockyRoad}
     
+    public static void main(String[] args) {
+        Flavor cone1, cone2, cone3;
+        
+        cone1 = Flavor.rockyRoad;
+        cone2 = Flavor.coffee;
+        
+        // enums have an ordinal value, i.e. what their position is in the enum
+        System.out.println("cone1 value: " + cone1);
+        System.out.println("cone1 ordinal: " + cone1.ordinal());
+        System.out.println("cone1 name: " + cone1.name());
+    }
 }
